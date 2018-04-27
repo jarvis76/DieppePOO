653ea74652b9bbcab452abaa12dcd2c04d3e6662
christopher <christopher@sfr.fr>
2018-04-27 15:14:24 +0200

    creation formulaire

---
+++ classes/Formulaire.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 27/04/2018
+ * Time: 14:36
+ */
+
+class Formulaire
+{
+    public function __construct $fomr= [])
+  {
+    if (!empty($form)
+    {
+        $this->hydrate($form;
+    }
+  }
+
+
+    public function label()
+    {
+        return $this->label;
+    }
+
+    public function name()
+
+    {
+        return $this->name;
+    }
+}
