e450991b4f5e3fb2aa5ba93b72e5963d755232a0
christopher <christopher@sfr.fr>
2018-05-02 10:32:01 +0200

    ajout du formaulaire

--- index.php
+++ index.php
@@ -23,24 +23,26 @@
 
 
 <?php
+define("PATHCONF", "./conf/");
 date_default_timezone_set("Europe/Paris");
 require_once "./functions/classAutoLoader.php";
 spl_autoload_register('classAutoloader');
 
 
 
-$toto = new Querie();
+//$toto = new Querie();
 
 /*Debug::dump($toto);
-die();*/
+die();
 if ($result = $toto->selectMethod("SELECT * FROM t_admin")) {
     Debug::dump($result);
 }
 else {
     echo "erreur";
-}
+}*/
 
-
+$test = new Form(PATHCONF, "registration");
+$test->frmGenerate();
 
 ?>
 
