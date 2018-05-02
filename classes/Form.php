019d7b6b66536ab39616a2e101dd709d73f212ca
christopher <christopher@sfr.fr>
2018-05-02 12:13:25 +0200

    mise a jour du formaulaire

--- classes/Form.php
+++ classes/Form.php
@@ -1,3 +1,4 @@
+
 <?php
 class Form
 {
@@ -8,12 +9,30 @@ class Form
         $this->path = $p;
         $this->file = $f;
     }
-    public function frmGenerate()
+    public function frmGenerate($actionURI)
     {
         $conf = parse_ini_file($this->path . $this->file . ".ini", true);
-        echo "<pre>";
-        print_r($conf);
-        echo "</pre>";
+        $form = "<form method='post' action='$actionURI'>";
+        foreach($conf as $content) {
+            $form .= "<div>";
+            if (isset($content['name'])) {
+                $form .= "<label for='" . $content['name'] . "'>";
+                $form .= ucfirst($content['name']);
+                $form .= "&nbsp;: ";
+                $form .= "</label>";
+            }
+            $form .= "<";
+            $form .= $content['tag'];
+            $form .= " ";
+            $form .= "type='";
+            $form .= $content['type'];
+            $form .= "' ";
+            $form .= isset($content['name']) ? "name='" . $content['name'] . "'" : "";
+            $form .= " />";
+            $form .= "</div>";
+        }
+        $form .= "</form>";
+        return $form;
     }
     public function frmCheck()
     {
