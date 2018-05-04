24ce64c0e0f0df3cb9b43a7bd5e0a2aeaa1c0b99
christopher <christopher@sfr.fr>
2018-05-04 09:03:58 +0200

    Ecriture fichier réception datas formulaire

--- form.php
+++ form.php
@@ -6,13 +6,10 @@
  * Time: 14:12
  */
 
-
-
-
-
 define("PATHCONF", "./conf/");
 date_default_timezone_set("Europe/Paris");
 require_once "./functions/classAutoLoader.php";
 spl_autoload_register('classAutoLoader');
 $test = new Form(PATHCONF, "registration");
-$test->frmCheck();
+echo $test->frmGenerate("form.php");
+
